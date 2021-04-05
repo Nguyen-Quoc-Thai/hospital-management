aeb8d2d3722f84807b9968b54dbd4ef69879dee4
mingtandev <tanlalan123@gmail.com>
2021-04-05 23:14:36 +0700

    Add material desig. Add template. Add MVVM Model

---
+++ PH1_QTCSDL/Command/UpdateViewCommand.cs
@@ -0,0 +1,33 @@
+﻿using PH1_QTCSDL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace PH1_QTCSDL.Command
+{
+    class UpdateViewCommand : ICommand
+    {
+        private MainViewModel _viewModel;
+        public UpdateViewCommand(MainViewModel viewModel)
+        {
+            this._viewModel = viewModel;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            switch(parameter)
+            {
+                case "A":
+
+                    break;
+            }
+        }
+    }
+}
