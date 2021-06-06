f324799d83ff732ff4637f442b4b22793a2832f1
MT0609 <thang123lk@gmail.com>
2021-06-06 19:52:29 +0700

    commit trc commit lon commit lai

---
+++ PH2_QTCSDL/Views/BacSi.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PH2_QTCSDL.Views
+{
+    /// <summary>
+    /// Interaction logic for TaiNguyen_NhanSu_View.xaml
+    /// </summary>
+    public partial class BacSi : UserControl
+    {
+        MainWindow main = (MainWindow)Application.Current.MainWindow;
+        public BacSi()
+        {
+            InitializeComponent();
+            ShowUserControl(HSBA);
+        }
+
+        private void QuanLiTaiVu_View(object sender, RoutedEventArgs e)
+        {
+            BacSi_View.Visibility = Visibility.Collapsed;
+            main.SetWindownActive(main.View_QuanLyTaiVu);       // NOT WORKING
+        }
+
+        public void ShowUserControl(UserControl uc)
+        {
+            HSBA.Visibility = Visibility.Collapsed;
+            CTDT.Visibility = Visibility.Collapsed;
+
+            uc.Visibility = Visibility.Visible;
+        }
+
+        private void TabSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = sender as TabControl;
+            var selected = item.SelectedItem as TabItem;
+            if (selected.Header.ToString() == "Hồ sơ bệnh án")
+                ShowUserControl(HSBA);
+            else if (selected.Header.ToString() == "Chi tiết đơn thuốc")
+                ShowUserControl(CTDT);
+        }
+    }
+}
---
+++ PH2_QTCSDL/Views/BacSi_Component/CTDT.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
+using System.Text.RegularExpressions;
+
+namespace PH2_QTCSDL.Views.BacSi_Component
+{
+    /// <summary>
+    /// Interaction logic for CTDT.xaml
+    /// </summary>
+    public partial class CTDT : UserControl
+    {
+        OracleDatabase db;
+        public CTDT()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateDataGrid();
+        }
+
+        private void UpdateDataGrid()
+        {
+            db = OracleDatabase.Instance;
+            OracleCommand cmd = db.CreateCommand("ALTER SESSION SET \"_ORACLE_SCRIPT\" = TRUE");
+            try
+            {
+                DataTable dt = db.Query("SELECT * FROM BS_VIEW_CHITIETDONTHUOC");
+                CTDT_table.ItemsSource = dt.DefaultView;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void CTDT_SOLUONG_NUM(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void CTDT_table_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dg = sender as DataGrid;
+            DataRowView dr = dg.SelectedItem as DataRowView;
+
+            if (dr != null)
+            {
+                CTDT_MAKB.Text = dr["MAKB"].ToString();
+                CTDT_MATHUOC.Text = dr["MATHUOC"].ToString();
+                CTDT_SOLUONG.Text = dr["SOLUONG"].ToString();
+                CTDT_LIEUDUNG.Text = dr["LIEUDUNG"].ToString();
+                CTDT_MOTA.Text = dr["MOTA"].ToString();
+            }
+        }
+
+        private void CTDT_update(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("Đì vé lợp pin");
+            }
+            catch
+            {
+                MessageBox.Show("Không thể chỉnh sửa bệnh án");
+            }
+        }
+    }
+}
