9a55853eb5af199b3a50a58016446afef3410a4c
Nguyen Quoc Thai <ct1.nquocthai@gmail.com>
2021-06-12 12:04:07 +0700

    system view

--- PH2_QTCSDL/Views/LoginFormView.xaml.cs
+++ PH2_QTCSDL/Views/LoginFormView.xaml.cs
@@ -28,23 +28,35 @@ namespace PH2_QTCSDL.Views
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            string connStr = "TNS_ADMIN=C:\\Users\\HP\\Oracle\\network\\admin;USER ID=" + txtUsername.Text + ";PASSWORD=" + txtPassword.Password + ";DATA SOURCE=localhost:1521/orcl;PERSIST SECURITY INFO=True";
+            //string connStr = "TNS_ADMIN=C:\\Users\\HP\\Oracle\\network\\admin;USER ID=" + txtUsername.Text + ";PASSWORD=" + txtPassword.Password + ";DATA SOURCE=localhost:1521/orcl;PERSIST SECURITY INFO=True";
+            string connStr = "TNS_ADMIN=C: \\Users\\Qouc Tahi\\Oracle\\network\\admin;USER ID=" + txtUsername.Text + ";PASSWORD=" + txtPassword.Password +";DATA SOURCE=localhost:1521/orclpdb1.localdomain;PERSIST SECURITY INFO=True";
+            
+            if(txtUsername.Text == "sys" || txtUsername.Text == "SYS")
+            {
+                connStr = "DBA PRIVILEGE = SYSDBA;" + connStr;
+            }
+
             OracleDatabase.connStr = connStr;
             OracleDatabase instance = OracleDatabase.Instance;
             try
-            { 
-                DataTable dt = instance.Query("select get_user_role from dual");
-                string role = dt.Rows[0]["GET_USER_ROLE"].ToString();
-                if (role == "BACSI")
-                    main.SetWindownActive(main.View_BacSi);
-                else if (role == "QLTAINGUYENNHANSU")
-                    main.SetWindownActive(main.View_TaiNguyen_NhanSu);
-                else if (role == "TIEPTAN")
-                    main.SetWindownActive(main.View_TiepTan);
-                else
+            {   if (txtUsername.Text == "sys" || txtUsername.Text == "SYS")
                 {
-                    MessageBox.Show("Bạn không có quyền vào hệ thống");
-                    instance = OracleDatabase.ResetInstance;
+                    main.SetWindownActive(main.View_Sys);
+                } else
+                {
+                    DataTable dt = instance.Query("select get_user_role from dual");
+                    string role = dt.Rows[0]["GET_USER_ROLE"].ToString();
+                    if (role == "BACSI")
+                        main.SetWindownActive(main.View_BacSi);
+                    else if (role == "QLTAINGUYENNHANSU")
+                        main.SetWindownActive(main.View_TaiNguyen_NhanSu);
+                    else if (role == "TIEPTAN")
+                        main.SetWindownActive(main.View_TiepTan);
+                    else
+                    {
+                        MessageBox.Show("Bạn không có quyền vào hệ thống");
+                        instance = OracleDatabase.ResetInstance;
+                    }
                 }
                 txtUsername.Clear();
                 txtPassword.Clear();
---
+++ PH2_QTCSDL/Views/Sys.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PH2_QTCSDL.Views
+{
+    /// <summary>
+    /// Interaction logic for Sys.xaml
+    /// </summary>
+    public partial class Sys : UserControl
+    {
+        MainWindow main = (MainWindow)Application.Current.MainWindow;
+        public Sys()
+        {
+            InitializeComponent();
+        }
+
+        public void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            OracleDatabase instance = OracleDatabase.ResetInstance;
+            main.SetWindownActive(main.View_Login);
+        }
+
+        public void ShowUserControl(UserControl uc)
+        {
+            Audit.Visibility = Visibility.Collapsed;
+
+            uc.Visibility = Visibility.Visible;
+        }
+
+        private void TabSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = sender as TabControl;
+            var selected = item.SelectedItem as TabItem;
+            if (selected.Header.ToString() == "Audit logs")
+                ShowUserControl(Audit);
+        }
+    }
+}
