8cad86a684eade749b48e42d9f3665658d0e83a6
Nguyen Quoc Thai <ct1.nquocthai@gmail.com>
2021-06-14 17:32:46 +0700

    add view sys

---
+++ PH2_QTCSDL/Views/Sys_Component/Policy.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PH2_QTCSDL.Views.Sys_Component
+{
+    /// <summary>
+    /// Interaction logic for Policy.xaml
+    /// </summary>
+    public partial class Policy : UserControl
+    {
+        OracleDatabase db;
+        public Policy()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.UpdateDataGrid();
+        }
+
+        private void UpdateDataGrid()
+        {
+            db = OracleDatabase.Instance;
+            OracleCommand cmd = db.CreateCommand("ALTER SESSION SET \"_ORACLE_SCRIPT\" = TRUE");
+            try
+            {
+                DataTable dt = db.Query("SELECT object_schema,object_name,policy_name,policy_column,enabled,sel,ins,upd,del FROM dba_audit_policies");
+                Policy_table.ItemsSource = dt.DefaultView;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
